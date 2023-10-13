e65a04728db68db7800c1406e1f3c5313313d9ec
MOHAMMAD SAIDUL ISLAM <isaidul92@gmail.com>
2023-10-13 22:29:06 +0600

    Step App done
    Step App done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
+
+export default function App() {
+    return <div>
+        <Steps />
+
     </div>
-  );
 }
 
-export default App;
+
+function Steps() {
+
+
+    const [stepCount, setStepCount] = useState(1);
+    const [isopen, setIsopen] = useState(true);
+
+    function handlePrvious() {
+        if (stepCount > 1) setStepCount(step => step - 1)
+    }
+    function handleNext() {
+        if (stepCount < messages.length) setStepCount(step => step + 1)
+    }
+
+    return (
+        <div>
+            <button className="close" onClick={() => setIsopen((is) => !is)}>
+                &times;
+            </button>
+
+            {
+
+
+                isopen ?
+                    (
+                        <div className="steps">
+                            <div className="numbers">
+                                <div className={stepCount >= 1 ? 'active' : ''}>1</div>
+                                <div className={stepCount >= 2 ? 'active' : ''}>2</div>
+                                <div className={stepCount >= 3 ? 'active' : ''}>3</div>
+                            </div>
+
+
+                            <StepMessage stepCount={stepCount}>
+
+                                <div className="buttons">
+                                    <Button
+
+                                        BgColor="#7950f2"
+                                        textColor="#fff"
+                                        onClickEvent={() => alert(`Learn how to ${messages[stepCount - 1]}`)}
+                                    >
+
+
+                                        Learn how
+                                    </Button>
+
+                                </div>
+                            </StepMessage>
+                            <div className="buttons">
+                                <Button
+
+                                    BgColor="#7950f2"
+                                    textColor="#fff"
+                                    onClickEvent={handlePrvious}
+
+                                >
+                                    Previous
+                                </Button>
+                                <Button
+                                    BgColor="#7950f2"
+                                    textColor="#fff"
+                                    onClickEvent={handleNext}
+
+                                >
+                                    Next
+                                </Button>
+                            </div>
+                        </div>
+                    )
+                    : <Button BgColor="#7950f2"
+                        textColor="#fff"
+                    >Click the cross button again to see the Steps</Button>
+
+            }
+        </div>
+    );
+}
+
+
+function StepMessage({ stepCount, children }) {
+    return <div className="message">
+        <h3>Step {stepCount}</h3>
+        {children}
+    </div>
+}
+
+
+function Button({ BgColor, textColor, children, onClickEvent }) {
+    return <button style={{ backgroundColor: BgColor, color: textColor }} onClick={onClickEvent}>
+        {children}
+    </button>
+}
+
+
